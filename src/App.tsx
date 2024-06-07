67c8ae784208c1741fcf769fd8d1fb4def57b635
jydxh <wzxxds@gmail.com>
2024-06-07 09:27:51 -0400

    done the login and register ui and logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from "./pages";
 
 import { action as LoginAction } from "@/pages/Login";
+import { action as RegisterAction } from "@/pages/Register";
 
 const router = createBrowserRouter([
 	{
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
 	{
 		path: "/register",
 		element: <Register />,
+		action: RegisterAction,
 	},
 ]);
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { ActionFunction, Form, redirect } from "react-router-dom";
 import { store } from "@/store";
 import { login } from "@/feature/User/userSlice";
+import { Link } from "react-router-dom";
 
 export const action: ActionFunction = async ({
 	request,
@@ -21,8 +22,17 @@ export const action: ActionFunction = async ({
 
 function Login() {
 	return (
-		<div className="grid place-content-center h-screen">
-			<Form method="POST" className=" border rounded-xl p-8 w-[30rem]">
+		<div
+			className="grid place-content-center h-screen"
+			style={{
+				background:
+					"url('https://www.themoviedb.org/t/p/w1280/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg')",
+				backgroundSize: "cover",
+				backgroundRepeat: "no-repeat",
+			}}>
+			<Form
+				method="POST"
+				className=" border rounded-xl p-8 w-[30rem] bg-slate-700">
 				<p className="text-center mx-auto text-3xl font-semibold">Login</p>
 				<div className="mt-8 flex justify-center items-center gap-x-4">
 					<label htmlFor="username" className="text-xl">
@@ -54,6 +64,17 @@ function Login() {
 					</Button>
 					<Button color="secondary" type="reset">
 						Reset
+					</Button>
+				</div>
+				<div className="flex  items-center justify-center gap-x-4">
+					<p>Not a member yet? </p>
+					<Button type="button">
+						<Link to="/register">Register</Link>
+					</Button>
+				</div>
+				<div className="flex justify-center mt-4">
+					<Button type="button" color="info">
+						<Link to="/">Back Home</Link>
 					</Button>
 				</div>
 			</Form>
