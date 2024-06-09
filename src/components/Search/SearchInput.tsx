f368199f8bc80dc7f4b2f4f5bc35b082d51a1ce1
jydxh <wzxxds@gmail.com>
2024-06-08 22:46:19 -0400

    add a serach Button for better UX at moblie devices

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,6 +1,7 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router";
+import { Button } from "@mui/material";
 function SearchInput() {
 	const { pathname, search } = useLocation();
 	const navigate = useNavigate();
@@ -12,8 +13,14 @@ function SearchInput() {
 		if (evt.key === "Enter") {
 			const query = evt.currentTarget.value;
 			params.set("query", query);
+			params.set("page", "1");
 			navigate(`${pathname}?${params.toString()}`);
 		}
+	};
+	const handleSubmit = () => {
+		params.set("query", inputValue);
+		params.set("page", "1");
+		navigate(`${pathname}?${params.toString()}`);
 	};
 	return (
 		<div className="border-b p-2 flex items-center gap-x-2">
@@ -29,6 +36,7 @@ function SearchInput() {
 				placeholder="Seach for a movie, tv show, person"
 				className="border border-gray-400 bg-slate-600 text-white px-4 py-1 w-full rounded italic"
 			/>
+			<Button onClick={handleSubmit}>Search</Button>
 		</div>
 	);
 }
