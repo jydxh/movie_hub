37eaf853337feb00128a8386cf3c7355d9ab71e8
jydxh <wzxxds@gmail.com>
2024-06-12 11:03:50 -0400

    1

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ import Home from "./Home";
 import Layout from "./Layout";
 import Login from "./Login";
 import Register from "./Register";
-import Movie from "./Movie";
+
 import TvShow from "./TvShow";
 import People from "./People";
 import SingleMovie from "./SingleMovie";
@@ -29,7 +29,6 @@ export {
 	Layout,
 	Login,
 	Register,
-	Movie,
 	TvShow,
 	People,
 	SingleMovie,
