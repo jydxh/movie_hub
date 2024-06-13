9a2dfbe5e2d8d86fd576822fe3954a2882818d24
jydxh <wzxxds@gmail.com>
2024-06-13 01:16:43 -0400

    fix ui

--- src/components/Movie/SelectableButton.tsx
+++ src/components/Movie/SelectableButton.tsx
@@ -19,8 +19,8 @@ function SelectableButton({
 		<button
 			type="button"
 			className={`${
-				genres.includes(id) ? "bg-teal-500 border-none" : ""
-			} hover:bg-teal-500 hover:border-none rounded-full border px-3 py-1`}
+				genres.includes(id) ? "bg-teal-500 border-teal-500" : ""
+			} hover:bg-teal-500 hover:border-teal-500 rounded-full border  px-3 py-1`}
 			onClick={handleClick}>
 			{children}
 		</button>
