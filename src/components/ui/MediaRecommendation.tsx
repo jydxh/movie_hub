509ccea23aea90dbabe8da3f0e86a8463607be03
jydxh <wzxxds@gmail.com>
2024-06-16 00:12:23 -0400

    1

--- src/components/ui/MediaRecommendation.tsx
+++ src/components/ui/MediaRecommendation.tsx
@@ -1,6 +1,7 @@
 import { Recommendations } from "@/utils/types";
 
 function MediaRecommendation(data: { data: Recommendations }) {
+	console.log(data);
 	return <div>MediaRecommendation</div>;
 }
 export default MediaRecommendation;
--- src/components/ui/MediaReview.tsx
+++ src/components/ui/MediaReview.tsx
@@ -1,6 +1,7 @@
 import { Reviews } from "@/utils/types";
 
 function MediaReview(data: { data: Reviews }) {
+	console.log(data);
 	return <div>MediaReview</div>;
 }
 export default MediaReview;
