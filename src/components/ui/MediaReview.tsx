29031012fb5509391d1ea562d511b5b1b9635fd9
jydxh <wzxxds@gmail.com>
2024-06-16 13:06:09 -0400

    done the movieReview part

--- src/components/ui/MediaReview.tsx
+++ src/components/ui/MediaReview.tsx
@@ -1,5 +1,5 @@
 import { Reviews } from "@/utils/types";
-
+import { Link } from "react-router-dom";
 import MediaReviewCard from "./MediaReviewCard";
 function MediaReview({ data, id }: { data: Reviews; id?: string }) {
 	console.log(data);
@@ -8,7 +8,30 @@ function MediaReview({ data, id }: { data: Reviews; id?: string }) {
 	console.log(data);
 
 	if (data) {
-		return <MediaReviewCard data={data} id={id} />;
+		return (
+			<section className="mt-4 p-4 ">
+				<div className="flex justify-start items-end gap-x-20">
+					<h3 className="font-semibold text-xl">Social</h3>
+					<p className="font-semibold">
+						Reviews <span className="font-light">0</span>
+					</p>
+				</div>
+				{data.results
+					.filter((_, index) => index === data.results.length - 1)
+					.map(result => (
+						<MediaReviewCard data={result} key={id} />
+					))}
+
+				<div
+					className={
+						data.results.length > 1
+							? "block mt-8 capitalize font-bold hover:text-gray-400"
+							: "hidden"
+					}>
+					<Link to={`/movie/${id}/reviews`}> read all reviews</Link>
+				</div>
+			</section>
+		);
 	} else {
 		return (
 			<section className="mt-4 p-4 ">
--- src/components/ui/MediaReviewCard.tsx
+++ src/components/ui/MediaReviewCard.tsx
@@ -1,61 +1,40 @@
 import formateDate from "@/utils/formateDate";
-import { Reviews, baseImgUrl } from "@/utils/types";
+import { baseImgUrl, ReviewsResult } from "@/utils/types";
 import convertTxtIntoPara from "@/utils/convertTxtIntoPara";
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
 
 export interface MediaReviewCardProps {
-	data: Reviews;
-	id?: string;
+	data: ReviewsResult;
 }
 
-function MediaReviewCard({ data, id }: MediaReviewCardProps) {
-	const { results } = data;
-	const latestReview = results[results.length - 1];
-	const { author, content, updated_at, url, author_details } = latestReview;
+function MediaReviewCard({ data }: MediaReviewCardProps) {
+	const { author, content, updated_at, url, author_details } = data;
 	return (
-		<section className="mt-4 p-4 ">
-			<div className="flex justify-start items-end gap-x-20">
-				<h3 className="font-semibold text-xl">Social</h3>
-				<p className="font-semibold">
-					Reviews <span className="font-light">{data.total_results}</span>
-				</p>
-			</div>
-			<div className="w-full border border-gray-200 shadow-lg mt-4 p-8 rounded-lg">
-				<div className="flex items-center justify-start gap-x-4">
-					<img
-						src={`${baseImgUrl}/w90_and_h90_face/${author_details.avatar_path}`}
-						alt={author}
-						className="rounded-full border-2 w-[3.8rem] h-[3.8rem]"
-					/>
-					<div className="flex flex-col justify-center">
-						<h5 className="font-bold text-xl">A review by {author}</h5>
-						<p className="font-light text-sm">
-							Written by <span className="font-bold">{author}</span> on{" "}
-							{formateDate(updated_at)}
-						</p>
-					</div>
-				</div>
-				<article
-					dangerouslySetInnerHTML={{
-						__html: convertTxtIntoPara(content),
-					}}
+		<div className="w-full border border-gray-200 shadow-lg mt-4 p-8 rounded-lg">
+			<div className="flex items-center justify-start gap-x-4">
+				<img
+					src={`${baseImgUrl}/w90_and_h90_face/${author_details.avatar_path}`}
+					alt={author}
+					className="rounded-full border-2 w-[3.8rem] h-[3.8rem]"
 				/>
-				{/* <p>{content}</p> */}
-				<Button size="small" variant="text">
-					<a href={url}>read the rest</a>
-				</Button>
+				<div className="flex flex-col justify-center">
+					<h5 className="font-bold text-xl">A review by {author}</h5>
+					<p className="font-light text-sm">
+						Written by <span className="font-bold">{author}</span> on{" "}
+						{formateDate(updated_at)}
+					</p>
+				</div>
 			</div>
-
-			<div
-				className={
-					results.length > 1
-						? "block mt-8 capitalize font-bold hover:text-gray-400"
-						: "hidden"
-				}>
-				<Link to={`/movie/${id}/reviews`}> read all reviews</Link>
-			</div>
-		</section>
+			<article
+				dangerouslySetInnerHTML={{
+					__html: convertTxtIntoPara(content),
+				}}
+			/>
+			{/* <p>{content}</p> */}
+			<Button size="small" variant="text">
+				<a href={url}>read the rest</a>
+			</Button>
+		</div>
 	);
 }
 export default MediaReviewCard;
